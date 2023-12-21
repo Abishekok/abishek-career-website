db128cc7cfaa79f0b98063dcaafb0208eb82eca5
Abishekok <93831950+Abishekok@users.noreply.github.com>
2023-12-21 13:48:02 +0000

    Added some UI code

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Chennai, India',
+    'salary': 'Rs.1000000'
+}, {
+    'id': 3,
+    'title': 'Python Developer',
+    'location': 'Madurai, India',
+    'salary': 'Rs.1000000'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs.1000000'
+}]
 
 
 @app.route("/")
 def hellp_abishek():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
